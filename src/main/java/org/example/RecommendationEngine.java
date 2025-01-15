0e28b831fb91325547425f7c17cacd2d1fe43ac5
fati12355 <133292699+fati12355@users.noreply.github.com>
2025-01-15 16:55:44 -0500

    Added the viewers and their rating for each movies in the database

--- src/main/java/org/example/RecommendationEngine.java
+++ src/main/java/org/example/RecommendationEngine.java
@@ -6,12 +6,15 @@ package org.example;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 // this is the class that will generate the recommendations for a user
 public class RecommendationEngine {
 
 	public ArrayList<Movie> moviesDatabase; //Create a database with all the movies
-
+	// Create a HashMap to store users by their ID
+	Map<Integer, User> usersDatabase = new HashMap<>();
 	public void readMovies(String csvFile) throws IOException,
 			NumberFormatException {
 
@@ -43,9 +46,85 @@ public class RecommendationEngine {
 			}
 
 			moviesDatabase.add(new Movie(movieID,title));
+
 		}
 
 	}
+	public void readRatings(String csvFile) throws IOException,
+			NumberFormatException {
+
+		String line;
+		String delimiter = ","; // Assuming values are separated by commas
+
+		BufferedReader br = new BufferedReader(new FileReader(csvFile));
+		// Read each line from the CSV file
+		line = br.readLine();
+
+		while ((line = br.readLine()) != null && line.length() > 0) {
+			// Split the line into parts using the delimiter
+			String[] parts = line.split(delimiter);
+
+			if (parts.length < 4)
+				throw new NumberFormatException("Error: Invalid line structure: " + line);// Each line
+			// should at least have the user and movie IDs, the rating and a timestamp
+
+			int currentUserID = Integer.parseInt(parts[0]); //ID of the user doing the rating
+			int movieID = Integer.parseInt(parts[1]); // ID of the movie being rated
+			Double rating = Double.parseDouble(parts[3]); // the rating score
+			ArrayList<Integer> likedMovies = new ArrayList<>(); // a list of all liked movies
+			ArrayList<Integer> unlikedMovies = new ArrayList<>(); // a list of all unliked movies
+			Double R = 3.5;
+			if (rating >= R) { // A rating should be higher than 3.5 to be considered a like
+				likedMovies.add(movieID);
+			}else{
+				unlikedMovies.add(movieID);
+			}
+			User currentUser = new User(currentUserID,likedMovies,unlikedMovies);
+			usersDatabase.put(currentUserID,currentUser); // Add the user in the database
+
+			//Add logic to populate the ratings for all movies
+			Movie movieToSetRatingFor; // Temporary container
+			HashMap<Integer, Double> ratingAndViewer = new HashMap<>();
+			ratingAndViewer.put(currentUserID, rating);
+			for ( int m =0; m < moviesDatabase.size(); m++){
+				movieToSetRatingFor = moviesDatabase.get(m);
+				if (movieToSetRatingFor.getMovieID() == movieID){
+					moviesDatabase.get(m).getViewersAndRatings().add(ratingAndViewer);
+					break;
+				}
+			}
+		}
+	}
+
+	//comment this part before test
+	public void findRecommendations(int targetUserID){ // the algorithm to find movies to recommend
+		//Get the list of seen movies for this user (liked and unliked movies)
+		ArrayList <Movie> seenMovies = usersDatabase.get(targetUserID).getLikedMovies();
+		ArrayList <Movie> unLikedMoviesToAdd = usersDatabase.get(targetUserID).getUnlikedMovies();
+		Movie dBsMovie; // temporary variable
+		int K = 10; // A movie should have been viewed by at least K users to be used for recommendation
+
+		for (int i=0; i< unLikedMoviesToAdd.size() ; i++ ){ // Combine the unliked movies to the liked one
+			seenMovies.add(unLikedMoviesToAdd.get(i));
+		}
+		for (int j = 0; j < moviesDatabase.size(); j++ ){ //for movies in the dataset
+			dBsMovie = moviesDatabase.get(j);
+			for ( int k = 0; k < seenMovies.size(); k++){
+				Movie currentMovie = seenMovies.get(k); // read the movies seen by the user one by one
+				if (currentMovie.getMovieID() == dBsMovie.getMovieID()){ //the user already saw the movie
+					break;
+				}else { // the user has not seen the movie yet. It may be recommended.
+					//check if the movie has been viewed by at least K users
+
+				}
+
+				}
+			}
+
+	}
+	//end of part to comment
+
+
 
 	public static void main(String[] args) {
 
